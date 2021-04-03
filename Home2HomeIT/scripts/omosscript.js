ea7bfa24f7de34c9236a0b5783abf4166f1ce94f
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2021-04-03 03:22:05 +0200

    soemthing

---
+++ Home2HomeIT/scripts/omosscript.js
@@ -0,0 +1,10 @@
+﻿function btnMenuOn() {
+    //document.getElementsByClassName("omosbody").style.display = "none";
+    var style1 = document.getElementById("omospopup");
+    style1.style.display = "grid";
+}
+
+function btnMenuOff() {
+    document.getElementById("omospopup").style.display = "none";
+    //document.getElementsByClassName("omosbody").style.display = "grid";
+}
